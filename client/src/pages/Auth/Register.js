7d8d56c04d82b0c7dcc552bb6c5733f771bf67fb
vinit mittal <vinitmittal295@gmail.com>
2025-01-01 12:27:59 +0530

    some file changes

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
             console.log(res.data);
             
             if (res.data.success) {
-                toast.success(res.data.message)
+                toast.success("register succesfully")
                 navigate("/login")
             } else {
                 toast.error(res.data.message)
@@ -49,6 +49,7 @@ const Register = () => {
   <div className="mb-3">
     <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" id="exampleInputPassword1"  placeholder='enter password' required/>
   </div>
+  
   <div className="mb-3">
     <input type="text" value={phone} onChange={(e)=>setPhone(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='enter phone' required />
   </div>
